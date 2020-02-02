c0f66e6de456170d1624ab16d4945830b3f43f36
marcosun <marcoinbristol@gmail.com>
2020-02-02 23:09:16 +0800

    [webpack] Disable CDN for local development.

--- webpack/webpack.local.js
+++ webpack/webpack.local.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackTemplate = require('html-webpack-template');
 const merge = require('webpack-merge');
 const path = require('./path');
 const common = require('./webpack.common.js');
@@ -11,4 +13,18 @@ module.exports = merge.smart(common, {
     contentBase: path.appSrc,
     historyApiFallback: true,
   },
+
+  externals: [],
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: HtmlWebpackTemplate,
+
+      title: 'react-ts-seed',
+      appMountId: 'app',
+      chunks: ['runtime', 'vendors', 'app'], // Specify javascript load order
+      chunksSortMode: 'manual',
+    }),
+  ],
 });
