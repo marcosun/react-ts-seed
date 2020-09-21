bee97e5eb653bfd8a3a5909e6c5df2573a4efb55
marcosun <marcoinbristol@gmail.com>
2020-09-21 23:09:59 +0800

    Upgrade dependencies to their latest

--- webpack/webpack.analyse.js
+++ webpack/webpack.analyse.js
@@ -1,8 +1,8 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const prod = require('./webpack.prod.js');
 
-module.exports = merge.smart(prod, {
+module.exports = merge(prod, {
   plugins: [
     new BundleAnalyzerPlugin(),
   ],
--- webpack/webpack.local.js
+++ webpack/webpack.local.js
@@ -1,10 +1,18 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackTemplate = require('html-webpack-template');
-const merge = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { mergeWithCustomize, customizeArray, customizeObject } = require('webpack-merge');
 const path = require('./path');
 const common = require('./webpack.common.js');
 
-module.exports = merge.smart(common, {
+module.exports = mergeWithCustomize({
+  customizeArray: customizeArray({
+    plugins: 'replace',
+  }),
+  customizeObject: customizeObject({
+    externals: 'replace',
+  }),
+})(common, {
   mode: 'development',
 
   devtool: 'eval-source-map',
@@ -14,9 +22,11 @@ module.exports = merge.smart(common, {
     historyApiFallback: true,
   },
 
-  externals: [],
+  externals: {},
 
   plugins: [
+    new CleanWebpackPlugin(),
+
     new HtmlWebpackPlugin({
       inject: false,
       template: HtmlWebpackTemplate,
